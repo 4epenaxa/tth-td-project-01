c2e27dd48623af88c158783fb5a8a8c2849cc052
4epenaxa <4epenaxa@mail.ru>
2017-05-14 23:02:45 +0300

    Cleaning up
    + added random

--- src/main/java/Jar.java
+++ src/main/java/Jar.java
@@ -1,18 +1,25 @@
+import java.util.Random;
+
 public class Jar {
   private String itemType;
   private int maxAmount;
   private int randomFromMaxAmount;
-  
+  private Random random;
+
   public String getItemType() {
     return itemType;
   }
   public int getMaxAmount() {
     return maxAmount;
   }
-  
+  public int getRandomFromMaxAmount() {
+    return randomFromMaxAmount;
+  }
+
   public Jar (String itemType, int maxAmount){
     this.itemType = itemType;
     this.maxAmount = maxAmount;
+    random = new Random();
+    this.randomFromMaxAmount = random.nextInt(maxAmount) + 1;
   }
-  
 }
--- src/main/java/Prompter.java
+++ src/main/java/Prompter.java
@@ -2,23 +2,27 @@ import java.util.Scanner;
 
 public class Prompter {
   private Jar jar;
-  Scanner scanner = new Scanner(System.in);
+  private Scanner scanner;
+
+  public Prompter() {
+    scanner = new Scanner(System.in);
+  }
 
   public void gettingData() {
-    
+
     System.out.printf("%nLETS PLAY%n-------------%n%nWhat type of item should go in the jar: ");
     String itemType = scanner.nextLine();
-    
+
     System.out.printf("What is the maximum amount of %s that should go in the jar: ",
-                     itemType);
-    String maxAmountAsString = scanner.nextLine();
-    int maxAmount = Integer.parseInt(maxAmountAsString);
-    
+            itemType);
+    int maxAmount = scanner.nextInt();
+
     jar = new Jar(itemType, maxAmount);
-    
-    System.out.printf("%n---------%n%n%n%n---------%n%n%s is your type%n%d is max amount%n",
-                      jar.getItemType(),
-                      jar.getMaxAmount());
+
+    System.out.printf("%n---------%n%n%n%n---------%n%n%s is your type%n%d is max amount%n%s is your random%n",
+            jar.getItemType(),
+            jar.getMaxAmount(),
+            jar.getRandomFromMaxAmount());
   }
-  
+
 }
