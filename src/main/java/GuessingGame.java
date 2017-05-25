d73fdf976ea9ef1eb2540a06e51031dc14c668f1
4epenaxa <4epenaxa@mail.ru>
2017-05-25 22:56:09 +0300

    v1.0
    only Prompter i/o fail

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,8 +1,52 @@
 public class GuessingGame {
-    public static void main(String[] args) {
+    private Prompter prompter;
+    private Jar jar;
+    private int guess = 0;
+    private int triesCount = 0;
 
-        Prompter prompter = new Prompter();
-        prompter.gettingData();
-        prompter.playingGame();
+    public static void main(String[] args) {
+        GuessingGame ggame = new GuessingGame();
+        ggame.play();
+    }
+
+    GuessingGame(){
+        prompter = new Prompter();
+        jar = prompter.jarPrompting();
+    }
+
+    public void play() {
+        boolean isWon = false;
+
+        while (!isWon) {
+            askForAGuess();
+
+            if (guess == jar.getRandomFromMaxAmount()) {
+                isWon = true;
+                prompter.winCongratulations(triesCount);
+            } else
+            if (guess < jar.getRandomFromMaxAmount()) {
+                prompter.guessIsLower();
+            } else
+            if (guess > jar.getRandomFromMaxAmount()) {
+                prompter.guessIsHigher();
+            }
+        }
+    }
+    private void askForAGuess() {
+        boolean isValid = false;
+
+        while (!isValid) {
+            try {
+                guess = Integer.valueOf(prompter.promptForAGuess(jar));
+                if (guess <= jar.getMaxAmount() && guess > 0) {
+                    isValid = true;
+                    triesCount++;
+                } else {
+                    prompter.againstZero(jar);
+                }
+            } catch (NumberFormatException nfe) {
+                prompter.incorrectInput();
+            }
+        }
     }
 }
--- src/main/java/Prompter.java
+++ src/main/java/Prompter.java
@@ -1,40 +1,77 @@
 import java.util.Scanner;
 
 public class Prompter {
-  private Jar jar;
   private Scanner scanner;
 
   public Prompter() {
     scanner = new Scanner(System.in);
   }
 
-  public void gettingData() {
-
+  public Jar jarPrompting() {
+    String itemType = "";
+    int maxAmount = 0;
     System.out.println("We need to setup this game, please answer for questions below:");
-    System.out.println("---------");
-    System.out.print("What type of item should go in the jar: ");
-    String itemType = scanner.nextLine();
-    System.out.printf("What is the maximum amount of %s that should go in the jar: ",
-            itemType);
-    int maxAmount = scanner.nextInt();
+    System.out.println("--------------------------------------------------------------");
 
-    jar = new Jar(itemType, maxAmount);
+    //DRY
+    do{
+      System.out.print("What type of item should go in the jar: ");
+      itemType = scanner.nextLine();
+      if (itemType.equals("")){
+        System.out.printf("Incorrect! Please try again!%n");
+      }
+    }
+    while (itemType.equals(""));
+
+    //DRY
+    boolean maxAmountable = false;
+    do {
+      System.out.printf("What is the maximum amount of %s that should go in the jar: ",
+              itemType);
+      String maxAmountS = scanner.nextLine();
+      try {
+        maxAmount = Integer.parseInt(maxAmountS);
+        if (maxAmount > 0){
+          maxAmountable = true;
+        } else { System.out.printf("Number must be > 0%n"); }
+
+      } catch(NumberFormatException e) {
+        System.out.printf("Incorrect! Please try again! You need to type a number.%n");
+      } catch(NullPointerException e) {
+        System.out.printf("Incorrect! Please try again! You need to type a number.%n");
+      }
+    } while (! maxAmountable);
+
+    System.out.printf("%n%nLETS PLAY%n---------%n");
+    return new Jar(itemType, maxAmount);
   }
 
-  public void playingGame() {
-    int choise;
-    int attempts = 0;
-    System.out.printf("%n%n%nLETS PLAY%n---------------------%n");
-    System.out.printf("How many %s are in the jar? Pick a number between 1 and %d.%n%n%n",
+  public String promptForAGuess (Jar jar) {
+    System.out.printf("How many %s are in the jar? Pick a number between 1 and %d: ",
             jar.getItemType(),
             jar.getMaxAmount());
-    do {
-      System.out.printf("Guess: ");
-      choise = scanner.nextInt();
-      attempts++;
-    } while (choise != jar.getRandomFromMaxAmount());
-
-    System.out.printf("You got it in %d attempt(s)%n", attempts);
+    return scanner.nextLine();
   }
 
+  public void guessIsLower() {
+    System.out.println("Your guess is too low");
+  }
+  public void guessIsHigher() {
+    System.out.println("Your guess is too high");
+  }
+  public void winCongratulations (int guessCount) {
+    System.out.println("Congratulations! You won!!!");
+    if (guessCount == 1) {
+      System.out.printf ("You got it in %d attempt.%n%n", guessCount);
+    } else {
+      System.out.printf ("You got it in %d attempts.%n%n", guessCount);
+    }
+  }
+
+  public void incorrectInput() {
+    System.out.println ("Incorrect input!");
+  }
+  public void againstZero(Jar jar) {
+    System.out.printf("Your guess must be between 1 and %d!%n", jar.getMaxAmount());
+  }
 }
